096e0d4e1683beeb6e518cecf27f4256eb6b97ad
Adarsh Sable <45851629+adarshsable03@users.noreply.github.com>
2021-04-13 01:12:45 +0530

    Create kelvinWeather.js

---
+++ Kelvin Weather/kelvinWeather.js
@@ -0,0 +1,7 @@
+const kelvin = 293;
+let celsius = kelvin-273;
+let fahrenheit = (celsius*(9/5))+32;
+
+Math.floor(fahrenheit);
+
+console.log(`the tempaertaure is ${fahrenheit} degrees Fahrenheit.`);
