9e461983dade253d1ee38295cf86b0e16f544bf1
Adarsh Sable <45851629+adarshsable03@users.noreply.github.com>
2021-04-13 01:10:54 +0530

    Create DogYears.js

---
+++ Dog Years/DogYears.js
@@ -0,0 +1,11 @@
+let myAge = 22;
+// age mine 
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+let laterYears = myAge-2;
+laterYears*4;
+let myAgeInDogYears = earlyYears+laterYears;
+myName = 'Adarsh'.toLowerCase();
+console.log(`My name is ${myName}.
+I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
