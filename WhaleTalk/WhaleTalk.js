8f943930d06f7df355050ef36c396777153744d1
Adarsh Sable <45851629+adarshsable03@users.noreply.github.com>
2021-05-10 00:24:55 +0530

    Create WhaleTalk.js

---
+++ WhaleTalk/WhaleTalk.js
@@ -0,0 +1,25 @@
+let input ='turpentine and turtles';
+const vowels =['a','e','i','o','u'];
+
+let resultArray= [];
+for(let i=0; i<input.length; i++){
+  for(let j=0; j<vowels.length; j++){
+    if(input[i]===vowels[j]){
+   
+      if(input[i]==='e' || input[i]==='u'){
+        resultArray.push(input[i],input[i]);
+  }
+  else{
+      resultArray.push([input[i]]);
+  }
+      }
+      
+    } 
+     
+}
+console.log(resultArray.join('').toUpperCase());
+
+
+
+
+
